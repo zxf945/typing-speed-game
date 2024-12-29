8cfd200c78c1d23fcc6eb2246e8a59f9477d2d1e
zxf945 <zxf-jlu@qq.com>
2024-12-29 11:42:00 +0800

    🔧 build: 引入prettier-tailwind插件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import  './App.css'
+import './App.css'
+
 export default function App() {
   return (
-    <h1 className="text-4xl 
-    font-bold underline text-purple-500">
+    <h1 className="text-4xl font-bold text-purple-500 underline">
       Hello world!2
     </h1>
   )
