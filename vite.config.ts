5d1518c67d7d81a18046f27166b6e1230c73369c
zxf945 <zxf-jlu@qq.com>
2025-01-31 20:01:45 +0800

    🔧 feat: 更新 Tailwind CSS 配置，移除不必要的文件并调整依赖版本

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
