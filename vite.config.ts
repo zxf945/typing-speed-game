b55f82ea01f1f688a06dcdb52f5398bcaf36647c
zxf945 <zxf-jlu@qq.com>
2025-04-05 16:48:32 +0800

    🔧 feat: 添加 code-inspector 插件以增强代码检查功能

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 import tailwindcss from '@tailwindcss/vite'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    codeInspectorPlugin({
+      bundler: 'vite',
+    }),
+  ],
 })
